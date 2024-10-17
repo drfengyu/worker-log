c2b0ef67d8c7baa240c4b85bda696b8e75257a92
drfengyu <98081672+drfengyu@users.noreply.github.com>
2024-10-17 13:56:56 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,18 +2,24 @@ export default {
   async fetch(request, env, ctx) {
     const clonedRequest = request.clone();
     try {
-    if (clonedRequest.method === 'POST') {
-      const requestBody = await clonedRequest.text();
-      console.log(`${requestBody}`);
+      if (clonedRequest.method === 'POST') {
+        const requestBody = await clonedRequest.text();
+        console.log(`${requestBody}`);
 
-      
         // 解析请求体为 JSON
         const data = JSON.parse(requestBody);
         const content = data.content;
 
         if (content != null) {
-          // 创建一个唯一的键，使用日期格式
-          const logKey = `log-${Date.now()}`;
+          // 使用当前时间作为键
+          const now = new Date();
+          const year = now.getFullYear();
+          const month = (now.getMonth() + 1).toString().padStart(2, '0');
+          const day = now.getDate().toString().padStart(2, '0');
+          const hours = now.getHours().toString().padStart(2, '0');
+          const minutes = now.getMinutes().toString().padStart(2, '0');
+          const seconds = now.getSeconds().toString().padStart(2, '0');
+          const logKey = `log-${year}${month}${day}${hours}${minutes}${seconds}`;
 
           try {
             // 使用 Cloudflare Workers 的 KV API 直接存储 content 值
@@ -23,10 +29,10 @@ export default {
             console.error(`Error storing content in KV: ${error.message}`);
           }
         }
-    }
-    }catch (error) {
-        console.error(`Error parsing JSON: ${error.message}`);
-        //return new Response('Invalid JSON', { status: 400 });
+      }
+    } catch (error) {
+      console.error(`Error parsing JSON: ${error.message}`);
+      //return new Response('Invalid JSON', { status: 400 });
     }
 
     try {
@@ -38,4 +44,5 @@ export default {
     }
   }
 };
+
 
